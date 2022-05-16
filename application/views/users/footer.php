7dab4c0add9e2dfdde18516c8362639c18a05ebb
rivaldoaldo <aldorivaldo190@gmail.com>
2022-05-16 09:47:58 +0800

    E-arsip

---
+++ application/views/users/footer.php
@@ -0,0 +1,17 @@
+<!-- Footer -->
+<div class="row">
+  <div class="col-md-12">
+    <p align="right"><b>E-ARSIP DISKOMINFO MAKASSAR &copy; 2021</b></p>
+  </div>
+</div>
+<!-- /footer -->
+<!-- /content area -->
+</div>
+<!-- /main content -->
+</div>
+<!-- /page content -->
+</div>
+<!-- /page container -->
+</body>
+
+</html>
